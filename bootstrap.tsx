2d05a96b9f67a5bbb164eb6f812b151961cc73fc
Sean <sean.snow@live.com>
2017-09-25 11:13:11 +0800

    不使用ThemeProvider

--- bootstrap.tsx
+++ bootstrap.tsx
@@ -1,10 +1,9 @@
 import * as React from 'react';
 import {render} from 'react-dom';
-import ThemeProvider from './ThemeProvider';
 
 export default (App, id = 'application'): any => new Promise((resolve) => {
   window.addEventListener('DOMContentLoaded', resolve);
 }).then(() => render(
-  <ThemeProvider><App/></ThemeProvider>,
+  <App/>,
   document.getElementById(id)
 ));
