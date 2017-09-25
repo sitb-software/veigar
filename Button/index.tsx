4081078cc0c687a7d2455487b91382ab048cc8db
Sean <sean.snow@live.com>
2017-09-25 13:08:15 +0800

    基础button功能

--- Button/index.tsx
+++ Button/index.tsx
@@ -7,22 +7,23 @@ import Component from '../AbstractComponent';
 import './index.scss';
 
 export interface Props {
-  children?: any,
-  mode: 'vertical' | 'horizontal',
+  mode?: 'primary' | 'default'
 }
 
 export default class Button extends Component<Props> {
+
   static defaultProps = {
-    mode: 'horizontal'
+    mode: 'default'
   };
+
   render() {
     const {children, mode, ...other} = this.props;
     return (
-      <div className={this.getClassName('buttonBox', mode)}>
-        <button {...other}
-                className={this.getClassName('button')}
-        >{children}</button>
-      </div>
+      <button {...other}
+              className={this.getClassName('button', mode)}
+      >
+        {children}
+      </button>
     );
   }
 
