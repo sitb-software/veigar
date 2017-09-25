ad24a9cd668c3b215835aa4ad00a866eb3f46d60
Sean <sean.snow@live.com>
2017-09-25 11:32:57 +0800

    字段验证

--- Input/index.tsx
+++ Input/index.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import Component from '../AbstractComponent';
+import AbstractFormItem from '../Form/AbstractFormItem';
 import Grid from '../Grid';
 import Column from '../Column';
 import {ItemProps} from '../Form/index';
@@ -8,18 +8,108 @@ import './index.scss';
 export interface Props extends ItemProps {
   label?: React.ReactNode;
   colon?: boolean,
+  /**
+   * 表明字段是必须填写的
+   */
   required?: boolean,
+  /**
+   * 当input没有填写值时提示的错误信息
+   */
+  missMsg?: string,
+  /**
+   * 正则表达式用于判断输入的值是否符合预期
+   */
   pattern?: RegExp,
-  errMsg?: string,
-  missMsg?: string
+  /**
+   * 与正则表达式不匹配错误消息
+   */
+  mismatchMsg?: string,
+  onChange?: Function,
+  onTextChange?: Function,
+  onBlur?: Function
 }
 
-export default class Input extends Component<Props, any> {
+/**
+ * 根据指定的props数组创建一组input输入框
+ * @param {Array<Props>} inputProps
+ * @returns {any[]}
+ */
+export function create<P extends Props>(inputProps: Array<P>) {
+  return inputProps.map((props, index) => (
+    <Input {...props}
+           key={index}
+    />
+  ));
+}
+
+export default class Input extends AbstractFormItem<Props, any> {
 
   static defaultProps = {
     mode: 'horizontal',
     colon: true
   };
+
+  state = {
+    value: ''
+  };
+
+  getValue(): any {
+    return this.state.value;
+  }
+
+  valid(): boolean {
+    const {
+      required, missMsg,
+      pattern, mismatchMsg,
+      name, form,
+    } = this.props;
+    const {value} = this.state;
+    if (required && value === '') {
+      console.warn(`${name} is required`);
+      form && form.putMissField(name, missMsg);
+      return false;
+    }
+
+    if (pattern && !pattern.test(value)) {
+      console.warn(`${name} mismatch ${pattern}`);
+      form && form.putMismatchField(name, mismatchMsg);
+      return false;
+    }
+
+    return true;
+  }
+
+  handleBlur(e) {
+    const {onBlur} = this.props;
+    onBlur && onBlur(e);
+  }
+
+  handleChange(e) {
+    const {value} = e.target;
+    const {onChange} = this.props;
+    this.handleTextChange(value);
+    onChange && onChange(e);
+  }
+
+  handleTextChange(value) {
+    const {type, onTextChange} = this.props;
+    switch (type) {
+      case 'bankCard':
+        value = value.replace(/\D/g, '').replace(/(....)(?=.)/g, '$1 ');
+        break;
+      case 'phone':
+        value = value.replace(/\D/g, '').substring(0, 11);
+        const valueLen = value.length;
+        if (valueLen > 3 && valueLen < 8) {
+          value = `${value.substr(0, 3)} ${value.substr(3)}`;
+        } else if (valueLen >= 8) {
+          value = `${value.substr(0, 3)} ${value.substr(3, 4)} ${value.substr(7)}`;
+        }
+        break;
+    }
+    this.setState({value: value});
+    onTextChange && onTextChange(value);
+  }
 
   renderLabel() {
     const {label, labelWidth, mode, colon} = this.props;
@@ -32,10 +122,6 @@ export default class Input extends Component<Props, any> {
     if (haveColon && typeof label === 'string' && (label as string).trim() !== '') {
       children = (label as string).replace(/[：|:]\s*$/, '');
     }
-
-    // const cls = {
-    //
-    // };
 
     return (
       <Column className={this.getClassName('label')}
@@ -60,6 +146,9 @@ export default class Input extends Component<Props, any> {
                type={type}
                placeholder={placeholder}
                name={name}
+               value={this.state.value}
+               onChange={this.handleChange}
+               onBlur={this.handleBlur}
         />
       </Column>
     );
