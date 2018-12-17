ede8f039272468a2131b483e8bee14d81a475c2c
Sean <sean.snow@live.com>
2018-12-17 17:11:31 +0800

    field 优化

--- Form/field.tsx
+++ Form/field.tsx
@@ -5,8 +5,63 @@ import { FormContext } from './index';
 /**
  * @author 田尘殇Sean(sean.snow@live.com) create at 2018/5/2
  */
-export default (ComposedComponent): any => (props: any) => (
-  <FormContext.Consumer>
-    {form => <ComposedComponent {...props} form={form}/>}
-  </FormContext.Consumer>
-);
+export default (ComposedComponent): any => class FieldWrapper extends React.Component<any, any> {
+
+  form;
+
+  /**
+   * 处理onChange事件
+   * @param event
+   */
+  handleChange(event) {
+    const {onChange, validate} = this.props;
+    const value = event.target.value;
+    this.setState({value});
+    onChange && onChange(event);
+    // 等于undefined 表明没有传值,则为true 开始校验Input
+    (validate === undefined || validate) && this.valid();
+  }
+
+  valid(): boolean {
+    const {
+      required, missText,
+      pattern, mismatchText,
+      name
+    } = this.props;
+
+    const {value} = this.state;
+    if (name && required && !value) {
+      console.warn(`${name} is required`);
+      this.form && this.form.putMissField(name, missText);
+      this.setState({miss: true});
+      return false;
+    }
+
+    if (pattern && !pattern.test(value)) {
+      console.warn(`${name} mismatch ${pattern}`);
+      this.form && this.form.putMismatchField(name, mismatchText);
+      this.setState({mismatch: true});
+      return false;
+    }
+
+    this.setState({miss: false, mismatch: false, error: false});
+    return true;
+  }
+
+  render() {
+    const {...props} = this.props;
+    return (
+      <FormContext.Consumer>
+        {form => {
+          this.form = form;
+          return (
+            <ComposedComponent {...props}
+                               form={form}
+                               onChange={this.handleChange}
+            />
+          );
+        }}
+      </FormContext.Consumer>
+    );
+  }
+}
